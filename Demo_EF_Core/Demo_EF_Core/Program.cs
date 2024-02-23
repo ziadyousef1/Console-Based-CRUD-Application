5b16522004a3649bf4db60bcc6a808f188041228
ziadyousef1 <120646398+ziadyousef1@users.noreply.github.com>
2024-02-23 21:53:23 +0200

    Add files via upload

---
+++ Demo_EF_Core/Demo_EF_Core/Program.cs
@@ -0,0 +1,17 @@
+﻿using Demo_EF_Core.Context;
+using Demo_EF_Core.Entitis;
+
+namespace Demo_EF_Core
+{
+    internal class Program
+    {
+
+
+        static void Main(string[] args)
+        {
+
+
+          
+        }
+    }
+}
